12161f61967c8e1198449f631c82d46a2d62eae6
Stefan Heber <stefan.heber@gmail.com>
2020-04-24 14:53:10 +0200

    updated the local config file

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,6 +1,39 @@
-;; (global-set-key (kbd "C-+") 'text-scale-increase)
-;; (global-set-key (kbd "C--") 'text-scale-decrease)
+(require-package 'use-package)
+
 ;; (global-set-key "\C-l" 'goto-line) ;; M-g g
+
+;; mouse-2 events by pressing Command when clicking with the trackpad.
+(when *is-a-mac*
+  (define-key key-translation-map (kbd "<S-mouse-1>") (kbd "<mouse-2>")))
+
+;; Highlights the current cursor line
+(global-hl-line-mode t)
+
+;; deletes all the whitespace when you hit backspace or delete
+(use-package hungry-delete
+  :ensure t
+  :config
+  (global-hungry-delete-mode))
+;; note M-x delete-trailing-whitespace will delete all trailing whitespace in the buffer.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; font scaling
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package default-text-scale
+  :ensure t
+  :config
+  (global-set-key (kbd "C-M-=") 'default-text-scale-increase)
+  (global-set-key (kbd "C-M--") 'default-text-scale-decrease))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; undo
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package undo-tree
+  :ensure t
+  :init
+  (global-undo-tree-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; python-mode
@@ -14,7 +47,6 @@
             (setq python-indent 4)))
 
 (add-to-list 'auto-mode-alist '("\\.pyx\\'" . python-mode))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; c++-mode
@@ -41,10 +73,17 @@
 ;;;; latex-mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; latex dict
-(add-hook 'latex-mode-hook 'flyspell-mode)
+(require-package 'auctex)
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+;; latex dictionary
+(add-hook 'LaTeX-mode-hook #'turn-on-flyspell)
 ;;(setq flyspell-issue-welcome-flag nil) ;; fix flyspell problem
 ;;(setq flyspell-issue-message-flag nil)
+
+;; flyspell-correct-word-generic C-c $
 
 ;; comment for latex
 ;;(defun uncomment-region (beg end)
@@ -66,15 +105,132 @@
 ;;;; neotree or speedbar
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Icons
-(require-package 'all-the-icons)
-;; run this command: M-x all-the-icons-install-fonts
+;; ;; Icons
+;; (require-package 'all-the-icons)
+;; ;; run this command: M-x all-the-icons-install-fonts
 
-(require-package 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-;;(setq neo-theme 'icons)
-;;(setq neo-theme (if (display-graphic-p) 'ascii))
-(setq neo-theme 'ascii)
-(setq neo-window-width 35)
+;; (require-package 'neotree)
+;; (global-set-key [f8] 'neotree-toggle)
+;; (setq neo-theme 'ascii)
+;; (setq neo-window-width 35)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; treemacs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :config
+  (progn
+
+    (setq treemacs-follow-after-init          t
+          treemacs-width                      35
+          treemacs-indentation                2
+          treemacs-git-integration            t
+          treemacs-collapse-dirs              3
+          treemacs-silent-refresh             nil
+          treemacs-change-root-without-asking nil
+          treemacs-sorting                    'alphabetic-desc
+          treemacs-show-hidden-files          t
+          treemacs-never-persist              nil
+          treemacs-is-never-other-window      nil
+          treemacs-goto-tag-strategy          'refetch-index)
+
+    (treemacs-resize-icons 44)
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t))
+  :bind
+
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ([f8]        . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+(use-package treemacs-persp
+  :after treemacs persp-mode
+  :ensure t
+  :config (treemacs-set-scope-type 'Perspectives))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; dictionary for iSpell
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq ispell-dictionary "english")
+
+(defun switch-to-english-dict ()
+  "Change dictionary to english."
+  (interactive)
+  (ispell-change-dictionary "english")
+  ;; (setq ispell-local-dictionary "english")
+  ;; (flyspell-mode 1)
+  (flyspell-buffer)
+  )
+
+(defun switch-to-german-dict  ()
+  "Change dictionary to german."
+  (interactive)
+  (ispell-change-dictionary "german")
+  ;; (setq ispell-local-dictionary "german")
+  ;; (flyspell-mode 1)
+  (flyspell-buffer)
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; org
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (use-package org-bullets
+;;              :ensure t
+;;              :init (add-hook 'org-mode-hook 'org-bullets-mode))
+(require-package 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; Automatically wrapping when you get to the end of a line
+;; note: this is not always a good idea (tables)
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; all-the-icons
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package all-the-icons
+  :ensure t
+  :defer 0.5)
+
+(use-package all-the-icons-ivy
+  :ensure t
+  :after (all-the-icons ivy)
+  :custom (all-the-icons-ivy-buffer-commands '(ivy-switch-buffer-other-window ivy-switch-buffer))
+  :config
+  (add-to-list 'all-the-icons-ivy-file-commands 'counsel-dired-jump)
+  (add-to-list 'all-the-icons-ivy-file-commands 'counsel-find-library)
+  (all-the-icons-ivy-setup))
+
+
+(use-package all-the-icons-dired
+  :ensure t
+  )
+
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+
 
 (provide 'init-local)
