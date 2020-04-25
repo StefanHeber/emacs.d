7c874a355c80248d07eb55e204c91c7c416ffa94
Stefan Heber <stefan.heber@gmail.com>
2020-04-25 15:28:34 +0200

    updated the local config and osx configs

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -3,8 +3,15 @@
 ;;; Code:
 
 (when *is-a-mac*
-  (setq mac-command-modifier 'meta)
-  (setq mac-option-modifier 'none)
+
+  ;; make option/alt-key meta
+  (setq mac-command-key-is-meta nil)
+  ;;(setq mac-command-modifier nil)
+  (setq mac-option-key-is-meta t)
+  (setq mac-option-modifier 'meta)
+
+  ;; (setq mac-command-modifier 'meta)
+  ;; (setq mac-option-modifier 'none)
   ;; Make mouse wheel / trackpad scrolling less jerky
   (setq mouse-wheel-scroll-amount '(1
                                     ((shift) . 5)
@@ -12,12 +19,12 @@
   (dolist (multiple '("" "double-" "triple-"))
     (dolist (direction '("right" "left"))
       (global-set-key (read-kbd-macro (concat "<" multiple "wheel-" direction ">")) 'ignore)))
-  (global-set-key (kbd "M-`") 'ns-next-frame)
-  (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
-  (global-set-key (kbd "M-˙") 'ns-do-hide-others)
+  (global-set-key (kbd "<mac-command-modifier>-`") 'ns-next-frame)
+  (global-set-key (kbd "<mac-command-modifier>-h") 'ns-do-hide-emacs)
+  (global-set-key (kbd "<mac-command-modifier>-˙") 'ns-do-hide-others)
   (after-load 'nxml-mode
     (define-key nxml-mode-map (kbd "M-h") nil))
-  (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
+  (global-set-key (kbd "<mac-command-modifier>-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
   )
 
 
