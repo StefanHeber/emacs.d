fc2615091ae7c9da2e4fae37fb3860001858e531
Stefan Heber <stefan@nakedlabs.com>
2021-01-07 09:43:05 +0100

    rtags changes

--- lisp/init-rtags.el
+++ lisp/init-rtags.el
@@ -19,8 +19,12 @@
     (function rtags-find-symbol-at-point))
   (define-key c-mode-base-map (kbd "M-,")
     (function rtags-find-references-at-point))
-  (define-key c-mode-base-map (kbd "M-*")
+  ;; (define-key c-mode-base-map (kbd "M-*")
+  ;;   (function rtags-location-stack-back))
+  (define-key c-mode-base-map (kbd "M-[")
     (function rtags-location-stack-back))
+  (define-key c-mode-base-map (kbd "M-]")
+    (function rtags-location-stack-forward))
   ;; disable prelude's use of C-c r, as this is the rtags keyboard prefix
   ;; (define-key prelude-mode-map (kbd "C-c r") nil)
   ;; install standard rtags keybindings. Do M-. on the symbol below to
@@ -34,14 +38,18 @@
   (setq rtags-completions-enabled t)
   ;; add company-rtags
   (when (maybe-require-package 'company-rtags)
-    (after-load 'company
-      (add-hook 'c-mode-common-hook
-                (lambda () (sanityinc/local-push-company-backend 'company-rtags)))))
+    (eval-after-load 'company
+      '(add-to-list
+        'company-backends 'company-rtags))
+    ;; (after-load 'company
+    ;;   (add-hook 'c-mode-common-hook
+    ;;             (lambda () (sanityinc/local-push-company-backend 'company-rtags))))
+    )
   ;;(push 'company-rtags company-backends)
   ;; (global-company-mode)
   ;;(define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
   ;; use rtags flycheck mode -- clang warnings shown inline
-  (require 'flycheck-rtags)
+  (require-package 'flycheck-rtags)
   ;; c-mode-common-hook is also called by c++-mode
   (add-hook 'c-mode-common-hook #'setup-flycheck-rtags)
   (add-hook 'c++-mode-hook #'setup-flycheck-rtags)
