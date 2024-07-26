0e975561ebf96d44a2c3af08e5e9b7f2b9957905
Stefan Heber <stefan.heber@gmail.com>
2024-07-26 10:23:33 +0200

    Updates for emacs 29.3

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,21 +1,18 @@
 ;;; init-eglot.el --- LSP support via eglot          -*- lexical-binding: t; -*-
 
 ;;; Commentary:
-
+;; python format settings are in .config/pycodestyle
 ;;; Code:
 
 ;; For Emacs >= 27
 ;; (setq read-process-output-max (* 1024 1024))
-
-;; eglot bug??
-;; (require 'text-property-search)
 
 (when (maybe-require-package 'eglot)
   (maybe-require-package 'consult-eglot))
 
 ;; Configure eglot-flymake-backend for C++ files
 (with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs '(c++-mode . ("clangd-10"))))
+  (add-to-list 'eglot-server-programs '(c++-mode . ("clangd"))))
 
 
 (provide 'init-eglot)
